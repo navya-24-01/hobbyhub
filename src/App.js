8b854551bee7188d0e4fd6514051a29c7337bb60
navya-24-01 <navyamunjal2401@gmail.com>
2024-03-02 18:41:02 +0530

    setting up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUpPage from "./Components/Pages/SignUpPage";
+import LogInPage from "./Components/Pages/LogInPage";
+import WelcomePage from "./Components/Pages/WelcomePage";
+import HomePage from "./Components/Pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" Component={WelcomePage} />
+        <Route exact path="/signup" Component={SignUpPage} />
+        <Route exact path="/signin" Component={LogInPage} />
+        <Route exact path="/home" Component={HomePage} />
+        </Routes>
+    </Router>
   );
 }
 
