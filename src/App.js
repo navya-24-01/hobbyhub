93bf768b4731ed729c60abb070e049328338ac38
Aman Desai <e0959108@u.nus.edu>
2024-04-04 15:37:43 +0800

    Payment
    Payment works

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import LogInPage from "./Components/Pages/LogInPage";
 import WelcomePage from "./Components/Pages/WelcomePage";
 import HomePage from "./Components/Pages/HomePage";
 import { AuthorizationProvider } from "./Context/AuthorizationContext";
+import CheckoutForm from "./Components/Pages/Checkout"
 
 function App() {
   return (
     <Router>
-        <AuthorizationProvider>
-          <Routes>
-        <Route exact path="/" Component={WelcomePage} />
-        <Route exact path="/signup" Component={SignUpPage} />
-        <Route exact path="/signin" Component={LogInPage} />
-        <Route exact path="/home" Component={HomePage} />
+      <AuthorizationProvider>
+        <Routes>
+          <Route exact path="/" Component={WelcomePage} />
+          <Route exact path="/signup" Component={SignUpPage} />
+          <Route exact path="/signin" Component={LogInPage} />
+          <Route exact path="/home" Component={HomePage} />
         </Routes>
-        </AuthorizationProvider>
+      </AuthorizationProvider>
+
+      <Routes>
+        <Route exact path='checkout/' index element={<CheckoutForm />} />
+      </Routes>
+
+
     </Router>
+
+
   );
 }
 
---
+++ src/Config/config.js
@@ -0,0 +1,4 @@
+export const CLIENT_ID = process.env.CLIENT_ID || "ASUEJZVsK_2SOAY-lzl49M39xbLSlkHc1cYvUv44WH2rsrYgUXc5S22DgNRD0jbVDDRzqGcMmEz3YrpC"
+export const APP_SECRET = process.env.APP_SECRET || "EI-5SN0WbkAQVaCjgXj94sTCgiWZtoBJVzlG0LBg8lCAWGWCrqHCXCKh_xu7Sth5v3YhDbHtoFmoMX2e"
+
+
