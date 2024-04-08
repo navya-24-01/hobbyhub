006d27539a742a9508d30ab5cb0a5c4091d1d5c2
qiaoling <seahqiaoling@gmail.com>
2024-04-08 17:26:02 +0800

    pulling listings from database

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUpPage from "./Components/Pages/SignUpPage";
 import WelcomePage from "./Components/Pages/WelcomePage";
 import HomePage from "./Components/Pages/HomePage";
-import Listings from "./Components/Pages/Listings";
 import { AuthorizationProvider } from "./Context/AuthorizationContext";
-import CheckoutForm from "./Components/Pages/Checkout";
+//import CheckoutForm from "./Components/Pages/Checkout";
 import SignInPage from "./Components/Pages/SignInPage";
+import ListingsPage from "./Components/Pages/Listings";
+import ListingDetails from "./Components/Pages/ListingDetails";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Route exact path="/signup" Component={SignUpPage} />
           <Route exact path="/signin" Component={SignInPage} />
           <Route exact path="/home" Component={HomePage} />
-          <Route exact path="/listings" Component={Listings} />
+          <Route path="/listings" element={<ListingsPage />} />
+          <Route path="/listing/:listingId" element={<ListingDetails />} />{" "}
         </Routes>
       </AuthorizationProvider>
     </Router>
