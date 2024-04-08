ff6891811cb48902cbc0e6b275239b82c1da23f2
Aman Desai <e0959108@u.nus.edu>
2024-04-08 18:00:41 +0800

    main errors solved

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./Components/Pages/HomePage";
 import { AuthorizationProvider } from "./Context/AuthorizationContext";
 //import CheckoutForm from "./Components/Pages/Checkout";
 import SignInPage from "./Components/Pages/SignInPage";
+import ReviewForm from "./Components/Pages/ReviewForm";
 
 import PrivateRoute from "./Components/Pages/PrivateRoute";
 import { ListingProvider } from "./Context/ListingContext";
@@ -23,10 +24,35 @@ function App() {
           <Route exact path="/signup" Component={SignUpPage} />
           <Route exact path="/signin" Component={SignInPage} />
 
-          <Route exact path="/home" Component={HomePage} />
-          <Route path="/listings" element={<ListingsPage />} />
+         
 
-          <Route exact path="/reviews" Component={ReviewForm} />
+
+
+
+          <Route
+                    path="/listings"
+                    element={
+                      <PrivateRoute>
+                        <ListingsPage />
+                      </PrivateRoute>
+                    }
+                  ></Route>
+
+
+<Route
+                    path="/reviews"
+                    element={
+                      <PrivateRoute>
+                        <ReviewForm />
+                      </PrivateRoute>
+                    }
+                  ></Route>
+
+
+                  
+
+
+          
 
           <Route
                     path="/home"
@@ -50,7 +76,7 @@ function App() {
                     path="/listings"
                     element={
                       <PrivateRoute>
-                        <Listings/>
+                        <ListingsPage/>
                       </PrivateRoute>
                     }
                   ></Route>
