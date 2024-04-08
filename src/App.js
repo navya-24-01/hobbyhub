cd944e58cbb7f7016e3aa1864ef74b4e94a71647
qiaoling <seahqiaoling@gmail.com>
2024-04-08 18:56:08 +0800

    seller's listings

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import SignUpPage from "./Components/Pages/SignUpPage";
 import WelcomePage from "./Components/Pages/WelcomePage";
 import HomePage from "./Components/Pages/HomePage";
 import { AuthorizationProvider } from "./Context/AuthorizationContext";
-//import CheckoutForm from "./Components/Pages/Checkout";
 import SignInPage from "./Components/Pages/SignInPage";
+import ReviewForm from "./Components/Pages/ReviewForm";
+import SellerListings from "./Components/Pages/SellerListings"; // Add this import
 
 import PrivateRoute from "./Components/Pages/PrivateRoute";
 import { ListingProvider } from "./Context/ListingContext";
@@ -12,52 +13,57 @@ import CreateListingPage from "./Components/Pages/CreateAListingPage";
 import ListingsPage from "./Components/Pages/Listings";
 import ListingDetails from "./Components/Pages/ListingDetails";
 
-
 function App() {
   return (
     <Router>
       <AuthorizationProvider>
         <ListingProvider>
-        <Routes>
-          <Route exact path="/" Component={WelcomePage} />
-          <Route exact path="/signup" Component={SignUpPage} />
-          <Route exact path="/signin" Component={SignInPage} />
-
-          <Route exact path="/home" Component={HomePage} />
-          <Route path="/listings" element={<ListingsPage />} />
-
-          <Route exact path="/reviews" Component={ReviewForm} />
-
-          <Route
-                    path="/home"
-                    element={
-                      <PrivateRoute>
-                        <HomePage />
-                      </PrivateRoute>
-                    }
-                  ></Route>
-        
-        <Route
-                    path="/createlisting"
-                    element={
-                      <PrivateRoute>
-                        <CreateListingPage />
-                      </PrivateRoute>
-                    }
-                  ></Route>
-
-<Route
-                    path="/listings"
-                    element={
-                      <PrivateRoute>
-                        <Listings/>
-                      </PrivateRoute>
-                    }
-                  ></Route>
-<Route path="/listing/:listingId" element={<ListingDetails />} />{" "}
-
-
-        </Routes>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/signin" element={<SignInPage />} />
+            <Route
+              path="/listings"
+              element={
+                <PrivateRoute>
+                  <ListingsPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/seller-listings"
+              element={
+                <PrivateRoute>
+                  <SellerListings />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/reviews"
+              element={
+                <PrivateRoute>
+                  <ReviewForm />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/createlisting"
+              element={
+                <PrivateRoute>
+                  <CreateListingPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/listing/:listingId" element={<ListingDetails />} />
+          </Routes>
         </ListingProvider>
       </AuthorizationProvider>
     </Router>
